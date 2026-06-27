f95c900cfb4ad83cf850547e088b0796dee8e40b
nikitafjsg-svg <nikitafjsg@gmail.com>
2026-06-27 21:59:32 +0700

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.VAPE_SUPABASE = {
-  url: 'YOUR_SUPABASE_URL',
-  anonKey: 'YOUR_SUPABASE_ANON_KEY'
+  url: 'https://pjxarmbbxusfelcxadrp.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqeGFybWJieHVzZmVsY3hhZHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0MDg4NTcsImV4cCI6MjA5Nzk4NDg1N30.0aTWkFQ-aXdO0O_2zuZyKOMeIoVZOw6I-vEIKhTueTU'
 };
